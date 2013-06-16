6ab4197795a24c4f084d4b6a7cea1d8b1f2b89b9
Vitaliy Gozhenko <vitaliy.gozhenko@gmail.com>
2013-06-17 02:15:16 +0300

    fixed autoresizing constant

--- Categories/UIView+Extra.h
+++ Categories/UIView+Extra.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
-#define kFullAutoresizingMask UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin 
+#define kFullAutoresizingMask UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight
 
 #define kExtraMinRatingTag		500
 #define kActivityViewTag		2983
