a0ccc848b0c15d46c9e32f30c4d452044cdd71fd
Vitaliy Gozhenko <gozhenko.vitaliy@gmail.com>
2014-11-02 19:50:27 +0200

    added UIColor from RGB function

--- AdditionalFunctions.h
+++ AdditionalFunctions.h
@@ -34,8 +34,9 @@ CG_INLINE BOOL isPhone()
 	return (BOOL)(deviceType() == kDeviceIPhone);
 }
 
+#define UIColorFromRGB(r, g, b)  [UIColor colorWithRed:((r) / 255.0) green:((g) / 255.0) blue:((b) / 255.0) alpha:1.0]
+
 #define MGLocalized(key)			NSLocalizedString(key, nil)
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
