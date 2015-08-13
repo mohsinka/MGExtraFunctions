ccef7ebcc55fa8afcfb21096e3f1f912c50ea163
Vitaliy Gozhenko <vitaliy.gozhenko@gmail.com>
2015-08-13 16:47:41 +0300

    added import for some headers

--- Categories/NSDate+Extra.h
+++ Categories/NSDate+Extra.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #define kNSDateOneMinuteInterval	(NSTimeInterval) (60)
 #define kNSDateOneHourInterval		(NSTimeInterval) (3600)
--- Controls/MGKeyboardViewExtension.m
+++ Controls/MGKeyboardViewExtension.m
@@ -7,6 +7,7 @@
 //
 
 #import "MGKeyboardViewExtension.h"
+#import "UIView+Extra.h"
 
 @interface MGKeyboardViewExtension ()
 
