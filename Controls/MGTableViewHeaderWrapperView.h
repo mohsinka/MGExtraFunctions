bc7c302b0cb7b45ab7a3bc2abf02a9235cfc7b3a
Vitaliy Gozhenko <vitaliy.gozhenko@gmail.com>
2015-09-01 22:14:54 +0300

    added class to wrap cell into table view section header/footer

---
+++ Controls/MGTableViewHeaderWrapperView.h
@@ -0,0 +1,15 @@
+//
+//  MGTableViewHeaderWrapperView.h
+//  GotMyJobs
+//
+//  Created by Vitaliy Gozhenko on 01/09/15.
+//  Copyright (c) 2015 GotMyJobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGTableViewHeaderWrapperView : UITableViewHeaderFooterView
+
+@property (strong, nonatomic) UITableViewCell *cell;
+
+@end
